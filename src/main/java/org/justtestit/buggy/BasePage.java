ebbc994f66f6198d42ef46b4398cabc8ec4362f4
saranyaK87 <ksaranya8@yahoo.com>
2023-05-07 19:27:30 +1200

    Cleaning code

--- src/main/java/org/justtestit/buggy/BasePage.java
+++ src/main/java/org/justtestit/buggy/BasePage.java
@@ -1,7 +1,14 @@
 package org.justtestit.buggy;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BasePage {
     protected WebDriver driver;
--- src/test/java/org/justtestit/buggy/BasePageTest.java
+++ src/test/java/org/justtestit/buggy/BasePageTest.java
@@ -3,6 +3,7 @@ package org.justtestit.buggy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -10,7 +11,11 @@ import org.testng.annotations.BeforeClass;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class BasePageTest {
@@ -37,6 +42,27 @@ public class BasePageTest {
     public static void setUp(){
 
         //System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/drivers/chrome/chromedriver.exe");
+
+
+        initDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+
+        driver.get(prop.getProperty("appurl"));
+        homePage = new HomePage(driver);
+
+    }
+
+    private static void initDriver(){
+        boolean runRemote = true;
+        if(runRemote){
+            initRemoteDriver();
+        }
+        initLocalDriver();
+    }
+
+    private static void initLocalDriver(){
         String driverPath = "C:\\Saranya\\Selenium\\chromedriver.exe";
 
         System.setProperty("webdriver.chrome.driver", driverPath);
@@ -45,13 +71,24 @@ public class BasePageTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
+    }
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.manage().window().maximize();
+    private static void initRemoteDriver(){
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 11");
+        browserOptions.setBrowserVersion("latest");
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("build", "selenium-build-PKRSI");
+        sauceOptions.put("name", "<your test name>");
+        browserOptions.setCapability("sauce:options", sauceOptions);
 
-        driver.get(prop.getProperty("appurl"));
-        homePage = new HomePage(driver);
-
+        URL url = null;
+        try {
+            url = new URL("https://oauth-ksaranya8-34e3f:643419d3-7a45-441f-af57-3284060972ad@ondemand.us-west-1.saucelabs.com:443/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver = new RemoteWebDriver(url, browserOptions);
     }
     @AfterMethod
     public void cleanUp(){
